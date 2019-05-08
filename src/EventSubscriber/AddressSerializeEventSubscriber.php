01d36e7deff3c1b9976c5796012d5a3515d90d13
Andreas Allacher <AndreasA@users.noreply.github.com>
2019-05-08 16:12:40 +0200

    Additional serializations (Address and Order) (#23)
    * Additional serializations
    
    * add fields optionally

---
+++ src/EventSubscriber/AddressSerializeEventSubscriber.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Sulu.
+ *
+ * (c) MASSIVE ART WebServices GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Sulu\SyliusProducerPlugin\EventSubscriber;
+
+use JMS\Serializer\EventDispatcher\Events;
+use JMS\Serializer\EventDispatcher\EventSubscriberInterface;
+use JMS\Serializer\EventDispatcher\ObjectEvent;
+use Sylius\Component\Addressing\Model\AddressInterface;
+use Sylius\Component\User\Model\UserInterface;
+
+class AddressSerializeEventSubscriber implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents()
+    {
+        return [
+            [
+                'event' => Events::POST_SERIALIZE,
+                'format' => 'json',
+                'method' => 'onPostSerialize',
+            ],
+        ];
+    }
+
+    public function onPostSerialize(ObjectEvent $event): void
+    {
+        $object = $event->getObject();
+        if (!$event->getContext()->hasAttribute('groups') || !$object instanceof AddressInterface) {
+            return;
+        }
+
+        $groups = $event->getContext()->getAttribute('groups');
+        if (!in_array('Detailed', $groups)) {
+            return;
+        }
+
+        if (null !== $object->getCompany()) {
+            $event->getVisitor()->setData('company', $object->getCompany());
+        }
+    }
+}
